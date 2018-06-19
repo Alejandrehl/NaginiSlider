6e9ea7d0ad49c2f42de0622f89abb367db1f4ef8
Alejandro <Alejandrehl@icloud.com>
2018-06-19 16:57:03 -0400

    Initial Commit

---
+++ NaginiSlide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NaginiSlide
+//
+//  Created by Alejandro Hernández Lara on 19-06-18.
+//  Copyright © 2018 Alejandro Hernández Lara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
