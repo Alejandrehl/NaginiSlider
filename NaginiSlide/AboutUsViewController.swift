a34b6121b590bb09e4d58616fae86bed1ff0e205
Alejandro <Alejandrehl@icloud.com>
2018-06-21 16:40:55 -0400

    webview cambiado por caja de texto , abous us funcionando para todos los dispositivos IOS

--- NaginiSlide/AboutUsViewController.swift
+++ NaginiSlide/AboutUsViewController.swift
@@ -18,15 +18,15 @@ class AboutUsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        if let url = Bundle.main.url(forResource: "NaginiSlide", withExtension: "html"){
-            
-            if let htmlData = try? Data(contentsOf: url){
-                
-                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
-                self.webView.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: baseURL)
-                
-            }
-        }
+//        if let url = Bundle.main.url(forResource: "NaginiSlide", withExtension: "html"){
+//            
+//            if let htmlData = try? Data(contentsOf: url){
+//                
+//                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+//                self.webView.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: baseURL)
+//                
+//            }
+//        }
     }
 
     override func didReceiveMemoryWarning() {
